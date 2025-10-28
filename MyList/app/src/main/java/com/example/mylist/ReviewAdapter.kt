13e2c5db934e64363fcfcde2ec805a6a6a59d04f
Marcos <marcosgb9@gmail.com>
2025-10-28 13:34:43 +0100

    1.5
    Cambios en el ReviewAdapter, y en los diseños de los layouts.

--- MyList/app/src/main/java/com/example/mylist/ReviewAdapter.kt
+++ MyList/app/src/main/java/com/example/mylist/ReviewAdapter.kt
@@ -4,9 +4,11 @@ import UserReview
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.RatingBar
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-
+import com.bumptech.glide.Glide
 
 class ReviewAdapter(
     private var reviews: List<UserReview>,
@@ -17,8 +19,9 @@ class ReviewAdapter(
         val titulo: TextView = itemView.findViewById(R.id.textTitulo)
         val genero: TextView = itemView.findViewById(R.id.textGenero)
         val comentario: TextView = itemView.findViewById(R.id.textComentario)
+        val ratingBar: RatingBar = itemView.findViewById(R.id.ratingBarItem)
+        val imageView: ImageView = itemView.findViewById(R.id.imageResena)
     }
-
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ReviewViewHolder {
         val view = LayoutInflater.from(parent.context)
@@ -26,16 +29,20 @@ class ReviewAdapter(
         return ReviewViewHolder(view)
     }
 
-
     override fun onBindViewHolder(holder: ReviewViewHolder, position: Int) {
         val review = reviews[position]
         holder.titulo.text = review.titulo
         holder.genero.text = review.genero
         holder.comentario.text = review.comentario
+        holder.ratingBar.rating = review.valoracion.toFloat()
 
-        holder.itemView.setOnClickListener {
-            onClick(review)
-        }
+        Glide.with(holder.itemView.context)
+            .load(review.imagen)
+            .centerCrop()
+            .placeholder(R.drawable.placeholder)
+            .into(holder.imageView)
+
+        holder.itemView.setOnClickListener { onClick(review) }
     }
 
 
